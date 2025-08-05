63b914a123c25fa9272c9565e3b2818d36d52107
SamsonGulma <sams0ngultek@gmail.com>
2025-08-05 16:38:42 +0300

    Claer the messup and Another initialization

--- desk_14/desk_14/asgi.py
+++ desk_14/desk_14/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for basicFunc project.
+ASGI config for desk_14 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basicFunc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'desk_14.settings')
 
 application = get_asgi_application()
--- desk_14/desk_14/wsgi.py
+++ desk_14/desk_14/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for basicFunc project.
+WSGI config for desk_14 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basicFunc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'desk_14.settings')
 
 application = get_wsgi_application()
